4e4f8c3fd854117c018e70b6e0dfce03b137e3c0
Hara Red <rtc6fg4.fejg2@gmail.com>
2024-05-09 16:02:32 +0300

    Apply Clippy lints to resource copying

--- devotee/src/input/winit_input.rs
+++ devotee/src/input/winit_input.rs
@@ -66,7 +66,7 @@ impl<'a, EventContext> Input<'a, EventContext> for Keyboard {
     }
 
     fn tick(&mut self) {
-        self.was_pressed = self.pressed.clone()
+        self.was_pressed.clone_from(&self.pressed)
     }
 }
 
@@ -165,7 +165,7 @@ where
     }
 
     fn tick(&mut self) {
-        self.was_pressed = self.pressed.clone();
+        self.was_pressed.clone_from(&self.pressed)
     }
 }
 
