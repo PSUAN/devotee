55e7f8f7af86c36747a73fd41c06b2838ce49c1f
Hara Red <rtc6fg4.fejg2@gmail.com>
2026-04-14 21:16:03 +0300

    Update dependencies

--- devotee/src/app/sound_system/rodio_sound_system.rs
+++ devotee/src/app/sound_system/rodio_sound_system.rs
@@ -1,57 +1,58 @@
 use std::rc::Rc;
 
-use rodio::{OutputStream, OutputStreamBuilder, Sink, Source, StreamError};
+use rodio::{DeviceSinkBuilder, DeviceSinkError, MixerDeviceSink, Player, Source};
 
 pub use rodio;
 
-/// Reference-counted `rodio` sink.
-pub type Sound = Rc<Sink>;
+/// Reference-counted `rodio` player.
+pub type Sound = Rc<Player>;
 
 /// Simple sound system implementation.
 pub struct SoundSystem {
-    output_stream: OutputStream,
-    sinks: Vec<Rc<Sink>>,
+    output_stream: MixerDeviceSink,
+    players: Vec<Sound>,
 }
 
 impl SoundSystem {
     /// Try creating new Sound System instance.
-    pub fn try_new() -> Result<Self, StreamError> {
-        let output_stream = OutputStreamBuilder::open_default_stream()?;
-        let sinks = Vec::new();
+    pub fn try_new() -> Result<Self, DeviceSinkError> {
+        let output_stream = DeviceSinkBuilder::open_default_sink()?;
+        let players = Vec::new();
         Ok(Self {
             output_stream,
-            sinks,
+            players,
         })
     }
 
-    fn free_sink(&mut self) -> Rc<Sink> {
-        if let Some(free_sink) = self.sinks.iter().find(|sink| sink.empty()) {
-            Rc::clone(free_sink)
+    fn free_player(&mut self) -> Sound {
+        if let Some(free_player) = self.players.iter_mut().find(|player| player.empty()) {
+            *free_player = Rc::new(Player::connect_new(self.output_stream.mixer()));
+            Rc::clone(free_player)
         } else {
-            let sink = Rc::new(Sink::connect_new(self.output_stream.mixer()));
-            self.sinks.push(Rc::clone(&sink));
-            sink
+            let player = Rc::new(Player::connect_new(self.output_stream.mixer()));
+            self.players.push(Rc::clone(&player));
+            player
         }
     }
 
     /// Play passed source and get `Sound` instance.
     pub fn play(&mut self, source: Box<dyn Source<Item = f32> + Send>) -> Sound {
-        let sink = self.free_sink();
-        sink.append(source);
-        sink
+        let player = self.free_player();
+        player.append(source);
+        player
     }
 
     /// Pause for all sounds playback.
     pub fn pause(&self) {
-        for sink in self.sinks.iter() {
-            sink.pause();
+        for player in self.players.iter() {
+            player.pause();
         }
     }
 
     /// Resume playback for all sounds.
     pub fn resume(&self) {
-        for sink in self.sinks.iter() {
-            sink.play();
+        for player in self.players.iter() {
+            player.play();
         }
     }
 }
